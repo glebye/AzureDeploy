1d310382147e4eae3b1be773a9c549d4cc2d9a91
glebye <glenn.ivar.bye@dustin.no>
2016-02-12 12:48:19 +0100

    uploaded JoinDomainAndInstallFeatures.ps1 script

---
+++ JoinDomainAndInstallFeatures.ps1
@@ -0,0 +1,116 @@
+﻿configuration DomainJoin 
+{ 
+   param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$domainName,
+
+        [Parameter(Mandatory)]
+        [PSCredential]$adminCreds
+    ) 
+    
+    Import-DscResource -ModuleName xActiveDirectory, xComputerManagement
+
+    $domainCreds = New-Object System.Management.Automation.PSCredential ("$domainName\$($adminCreds.UserName)", $adminCreds.Password)
+   
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature ADPowershell
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        } 
+
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $domainName
+            Credential = $domainCreds
+            DependsOn = "[WindowsFeature]ADPowershell" 
+        }
+   }
+}
+
+
+
+configuration JoinDomainAndInstallFeatures
+{
+   param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$domainName,
+
+        [Parameter(Mandatory)]
+        [PSCredential]$adminCreds
+    ) 
+
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+            ConfigurationMode = "ApplyOnly"
+        }
+
+        DomainJoin DomainJoin
+        {
+            domainName = $domainName 
+            adminCreds = $adminCreds 
+        }
+
+        WindowsFeature Remote-Desktop-Services 
+        { 
+            Ensure = "Present" 
+            Name = "Remote-Desktop-Services" 
+        }
+
+        WindowsFeature RSAT-RDS-Tools
+        {
+            Ensure = "Present"
+            Name = "RSAT-RDS-Tools"
+            IncludeAllSubFeature = $true
+        }
+
+        WindowsFeature DesktopExperience
+        { 
+            Ensure = "Present" 
+            Name = "Desktop-Experience"
+        }
+
+        WindowsFeature RDS-Connection-Broker 
+        { 
+            Ensure = "Present" 
+            Name = "RDS-Connection-Broker" 
+        }
+
+        WindowsFeature RDS-Licensing
+        {
+            Ensure = "Present"
+            Name = "RDS-Licensing"
+        }
+
+        WindowsFeature RDS-Gateway
+        {
+            Ensure = "Present"
+            Name = "RDS-Gateway"
+        }
+
+        WindowsFeature RDS-Web-Access
+        {
+            Ensure = "Present"
+            Name = "RDS-Web-Access"
+        }
+
+        WindowsFeature RDS-RD-Server
+        {
+            Ensure = "Present"
+            Name = "RDS-RD-Server"
+        }
+    }
+}
